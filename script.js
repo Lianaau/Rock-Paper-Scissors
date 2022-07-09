a46970e3662a821ef1f12d3451561245e667843b
Lianaau <93110675+Lianaau@users.noreply.github.com>
2022-07-08 22:23:28 -0700

    Version 2
    Changed player choice to lowercase

--- script.js
+++ script.js
@@ -1,71 +1,92 @@
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const playerChoiceDisplay = document.getElementById("player-choice");
+const resultDisplay = document.getElementById("result"); 
 
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computerChoice
-let result
+const computerScoreDisplay = document.getElementById("computer-score");
+const playerScoreDisplay = document.getElementById("player-score");
+const finalScore = document.getElementById("final"); 
 
-//Create a computerPlay function that will randomly return either 'Rock', 'Paper', or 'Scissors'
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
-    getResult()
-}))
+let playerChoice; 
+let computerChoice; 
+let result 
 
-function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random() * 3) + 1
+let playerScore = 0;  
+let computerScore = 0; 
+let final
 
-    if (randomNumber === 1) {
-        computerChoice = 'ROCK'
-    }
-    if (randomNumber === 2) {
-        computerChoice = 'SCISSORS'
-    }
-    if (randomNumber === 3) {
-        computerChoice = 'PAPER'
-    }
-    computerChoiceDisplay.innerHTML = computerChoice
+function handleClick(element){
+    playerChoice = window.prompt("Rock Paper or Scissors?"); 
+    playerChoice = playerChoice.toLowerCase(); 
+    playerChoiceDisplay.innerHTML = playerChoice;
+    element.innerHTML = "Play Again"
+    computerSelection(); 
+    playRound(); 
+    score(); 
+    endGame(); 
 }
 
-function getResult(){
-    if (computerChoice === userChoice){
+function computerSelection() {
+    const randomNumber = Math.floor(Math.random()*3) + 1; 
+
+    if (randomNumber === 1){
+        computerChoice = "rock"; 
+    }
+    if (randomNumber === 2){
+        computerChoice = "paper"; 
+    }
+    if (randomNumber === 3){
+        computerChoice = "scissors";
+    }
+    computerChoiceDisplay.innerHTML = computerChoice; 
+}
+
+function playRound(){
+    if (playerChoice === computerChoice){
         result = "It's a Draw!"
     }
-    if (computerChoice === 'ROCK' && userChoice === 'PAPER') {
+    else if (playerChoice === "rock" && computerChoice === "scissors" ){
         result = "You Win!"
     }
-    if (computerChoice === 'ROCK' && userChoice === 'SCISSORS') {
-        result = "You Lose!"
-    }
-    if (computerChoice === 'PAPER' && userChoice === 'SCISSORS') {
+    else if (playerChoice === "paper" && computerChoice === "rock"){
         result = "You Win!"
     }
-    if (computerChoice === 'PAPER' && userChoice === 'ROCK') {
-        result = "You Lose!"
-    }
-    if (computerChoice === 'SCISSORS' && userChoice === 'ROCK') {
+    else if (playerChoice=== "scissors" && computerChoice === "paper"){
         result = "You Win!"
     }
-    if (computerChoice === 'SCISSORS' && userChoice === 'PAPER') {
+    else if (playerChoice === "rock" && computerChoice === "paper"){
         result = "You Lose!"
+    }
+    else if (playerChoice === "paper" && computerChoice === "scissors"){
+        result = "You Lose!"
+    }
+    else if (playerChoice === "scissors" && computerChoice === "rock"){
+        result = "You Lose!"
+    } else {
+        result = "You did not enter a valid choice"
     }
     resultDisplay.innerHTML = result
 }
-/* 
--Write a function that plays a single round, takes two parameters: playerSelection and computerSelection
--return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
--Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation) */ 
+
+function score(){
+    if (result === "You Win!"){
+        playerScore += 1;      
+    }
+    if (result === "You Lose!"){
+        computerScore += 1;        
+    }
+    playerScoreDisplay.innerHTML = playerScore;
+    computerScoreDisplay.innerHTML = computerScore; 
+}    
+
+function endGame(){
+    if (playerScore === 3) {
+        final = "You are the Winner!"
+        finalScore.innerHTML = final;
+    } else if (computerScore === 3){
+        final = "You are the Loser!"
+        finalScore.innerHTML = final;
+    }
+     
+}
 
 
-// console.log(playerSelection); 
-// Return message if user didn't input correct choices 
-
-
- 
-// Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end. 
-
-
-// function game(){
